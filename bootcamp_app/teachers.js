761669be3f36ec35a16e5835c56286c398b4669e
Anchen Huang <acmhuang@gmail.com>
2019-07-25 01:22:09 +0000

    parameterized query

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -5,15 +5,19 @@ const pool = new Pool({
   database: "bootcampx"
 });
 
+const cohortName = process.argv[2] || "JUL02";
+// Store all potentially malicious values in an array.
+const values = [`'%${cohortName}%'`];
+
 pool
   .query(
-    `
+    values`
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2] || "JUL02"}'
+WHERE cohorts.name = $1
 ORDER BY teacher;
 `
   )
